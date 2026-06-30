1afee1e0cc4adb4f12cb1855b3be036779d69c66
minusone <sgone2842@gmail.com>
2026-06-30 11:44:20 +0530

    feat: add rate limit on active websockets

--- backend/src/main/java/com/mns/asyncscale/websocket/TelemetryHandler.java
+++ backend/src/main/java/com/mns/asyncscale/websocket/TelemetryHandler.java
@@ -25,20 +25,32 @@ public class TelemetryHandler extends TextWebSocketHandler {
 
     private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Object sessionLock = new Object();
     private final ExecutorService telemetryExecutor = Executors.newSingleThreadExecutor();
+    private final int MAX_CONCURRENT_SOCKETS = 10;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         String clientId = session.getUri().getQuery();
 
-         if (clientId == null || clientId.isBlank()) {
+        if (clientId == null || clientId.isBlank()) {
             session.close(CloseStatus.BAD_DATA.withReason("Client ID missing"));
             return;
         }
-        if(isUserConnected(clientId)){
-            return;
+
+        // atomic access to verify and put session in map
+        synchronized(sessionLock) {
+            if(isUserConnected(clientId)){
+                session.close(CloseStatus.POLICY_VIOLATION.withReason("Client already has a running session"));
+                return;
+            }
+            if(sessions.size() >= MAX_CONCURRENT_SOCKETS) {
+                session.close(CloseStatus.POLICY_VIOLATION.withReason("Server reached max clients, try again later"));
+                return;
+            }
+            sessions.put(clientId, session);
         }
-        sessions.put(clientId, session);
+
         log.info("User Connected! {}", clientId);
     }
 
@@ -54,6 +66,7 @@ public class TelemetryHandler extends TextWebSocketHandler {
         log.info("User Disconnected! {}", clientId);
     }
 
+    // add scoket disconnection request to queue and return immediately
     public void disconnectClient(String clientId) {
         telemetryExecutor.execute(() -> closeClientSession(clientId));
     }
@@ -91,7 +104,7 @@ public class TelemetryHandler extends TextWebSocketHandler {
                 String json = objectMapper.writeValueAsString(message);
                 clientSession.sendMessage(new TextMessage(json));
             } catch (IOException e) {
-                e.printStackTrace();
+                log.error("Failed to send telemetry", e);
             }
         }
 
